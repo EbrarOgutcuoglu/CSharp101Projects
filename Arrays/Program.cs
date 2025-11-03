13641c95704ac096539edb176555cdb0ccfabaef
EbrarOgutcuoglu <ebrarogutcuoglu@gmail.com>
2025-11-03 23:58:15 +0300

    Add array method examples

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -14,25 +14,66 @@
         //Utilization of Loops in Arrays
         //Calculates the average of n numbers
 
-        Console.Write("please sign a number: ");
-        int n = int.Parse(Console.ReadLine());
-        int[] numbers = new int[n];
-        int total = 0;
+         Console.Write("please sign a number: ");
+         int n = int.Parse(Console.ReadLine());
+         int[] numbers = new int[n];
+         int total = 0;
 
-        for (int i = 0;i<numbers.Length;i++)
-        {
-            Console.Write("please sign " + (i+1) + "th element ");
-            int num  = int.Parse(Console.ReadLine());
-            numbers[i] = num;
-      
-        }
-        for (int i = 0; i < numbers.Length; i++)
-        {
-            total += numbers[i];
+         for (int i = 0;i<numbers.Length;i++)
+         {
+             Console.Write("please sign " + (i+1) + "th element ");
+             int num  = int.Parse(Console.ReadLine());
+             numbers[i] = num;
 
+         }
+         for (int i = 0; i < numbers.Length; i++)
+         {
+             total += numbers[i];
+
+         }
+         double average = total / numbers.Length;
+         Console.WriteLine("Average: " + average);
+        
+
+
+        // Method of array class
+        //Sort
+
+        int[] numbers2 = { 2, 43, 5, 67, 18, 23 ,32,99};
+        Array.Sort(numbers2);
+        foreach (var item in numbers2)
+        {
+            Console.Write(item +" ");
         }
-        double average = total / numbers.Length;
-        Console.WriteLine("Average: " + average);
+        Console.WriteLine();
+
+        //Clear
+        Array.Clear(numbers2, 2,2);
+        foreach (var item in numbers2)
+        {
+            Console.Write(item + " ");
+        }
+        Console.WriteLine();
+
+        //Reverse
+        Array.Reverse(numbers2);
+        foreach (var item in numbers2)
+        {
+            Console.Write(item + " ");
+        }
+        Console.WriteLine();
+
+        //Indexof
+        Console.WriteLine("Index of 43: " + Array.IndexOf(numbers2,43));
+
+        //Resize
+        Array.Resize<int>(ref numbers2, 9);
+        numbers2[8] = 10;
+        foreach (var item in numbers2)
+        {
+            Console.Write(item + " ");
+        }
+
 
     }
 }
