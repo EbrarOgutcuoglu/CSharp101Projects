c18a1b7bcc894f470db347bc03d4f94936d76600
EbrarOgutcuoglu <ebrarogutcuoglu@gmail.com>
2025-11-29 07:19:51 +0300

    ArrayList is completed

---
+++ ArrayList/Program.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        ArrayList list = new ArrayList();
+        list.Add("Ebrar");
+        list.Add(true);
+        list.Add('H');
+
+        Console.WriteLine("***** ArrayList *****");
+        Console.WriteLine(list[1]);
+
+        foreach (var item in list)
+        {
+            Console.WriteLine(item);
+        }
+
+        //AddRange
+        Console.WriteLine("**** Add Range ****");
+
+        string[] colors = { "kırmızı", "sarı", "yeşil" };
+
+        List<int> numbers = new List<int>() { 1, 11, 3, 7, 9, 92, 5 };
+
+        list.AddRange(colors);
+        list.AddRange(numbers);
+
+        foreach (var item in list)
+        {
+            Console.WriteLine(item);
+        }
+
+        //Sort
+        Console.WriteLine("**** Sort ****");
+
+        List<int> orderedlist = new List<int>() { 1, 8, 3, 7, 9, 92, 5 };
+        orderedlist.Sort();
+
+        foreach (var item in orderedlist)
+        {
+            Console.WriteLine(item);
+        }
+
+        
+        //Binary Search
+        Console.WriteLine("**** Binary Search ****");
+
+        int index = orderedlist.BinarySearch(9);
+
+        Console.WriteLine(index);
+
+        //Reverse
+        Console.WriteLine("***Reverse****");
+        orderedlist.Reverse();
+
+        foreach (var item in orderedlist)
+        {
+            Console.WriteLine(item);
+        }
+
+
+        //clear
+        Console.WriteLine("****Clear****");
+        orderedlist.Clear();
+        foreach (var item in orderedlist)
+        {
+            Console.WriteLine(item);
+        }
+    }
+}
